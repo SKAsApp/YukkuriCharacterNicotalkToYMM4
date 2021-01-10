cd7e8cd90ab5bd2e2fe20a1a98dc8ef7499603d7
SKAsApp <60283197+SKAsApp@users.noreply.github.com>
2021-01-11 00:52:14 +0900

    2021/01/11　UI作成

---
+++ YukkuriCharacterNicotalkToYMM4/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページの項目テンプレートについては、https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x411 を参照してください
+
+namespace YukkuriCharacterNicotalkToYMM4
+{
+	/// <summary>
+	/// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+	/// </summary>
+	public sealed partial class MainPage: Page
+	{
+		public MainPage( )
+		{
+			this.InitializeComponent( );
+		}
+
+		private void CommandCopyClick(object sender, RoutedEventArgs e)
+		{
+			
+		}
+
+		private void CommandPasteClick(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void CommandAboutClick(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ButtonInputClick(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ButtonOutputClick(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		private void ButtonRunClick(object sender, RoutedEventArgs e)
+		{
+
+		}
+
+		
+	}
+}
---
+++ YukkuriCharacterNicotalkToYMM4/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は、以下の属性セットによって制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更します。
+[assembly: AssemblyTitle("YukkuriCharacterNicotalkToYMM4")]
+[assembly: AssemblyDescription("キャラ素材をnicotalk形式からゆっくりムービーメーカー4形式に変換するヤツ")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("YukkuriCharacterNicotalkToYMM4")]
+[assembly: AssemblyCopyright("Copyright © SKA’s App 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 既定値にすることができます:
+//[アセンブリ: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("ja-JP")]
